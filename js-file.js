f2bb31a7bafc17d2bf8272636851bf7c42fc9f4c
chiually <allyssa.chiu@mail.utoronto.ca>
2025-06-15 02:09:16 -0400

    implemented interactivity

--- js-file.js
+++ js-file.js
@@ -42,10 +42,11 @@ const GameBoard = (function() {
 
 // X represented by 1, O represented by 0
 // contains game logic and flow 
-const GameController = (function(playerOneName = "player1", playerTwoName = "player2") {
+function GameController(playerOneName = "player1", playerTwoName = "player2") {
 
     const player1 = createPlayer(playerOneName, "O");
     const player2 = createPlayer(playerTwoName, "X");
+    let msg = "";
 
     let activePlayer = player1;
     let counter = 0;
@@ -55,19 +56,22 @@ const GameController = (function(playerOneName = "player1", playerTwoName = "pla
     const switchActivePlayer = () => activePlayer = (activePlayer === player1) ? player2 : player1;
 
     const printNewRound = () => {
-        console.log(activePlayer.name + "'s turn!")
+        msg = activePlayer.name + "'s turn!";
+        console.log(msg);
         GameBoard.printBoard();
+        return msg;
     };
+
+    const getMsg = () => msg;
 
     const playRound = function(location) {
         // if move was legal 
         if (GameBoard.writeMoveToBoard(activePlayer, location)) {
 
             if (checkForWin(location)) {
-                console.log(`${activePlayer.name} is the winner!`)
+                msg = `${activePlayer.name} is the winner!`;
+                console.log(msg)
                 GameBoard.printBoard();
-                GameBoard.clearBoard();
-                counter = 0;
                 return;
             }
 
@@ -76,12 +80,20 @@ const GameController = (function(playerOneName = "player1", playerTwoName = "pla
         }
 
         if (counter === 9) {
-            console.log("No winner");
+            msg = "No winner";
+            console.log(msg);
         }
         else {
             printNewRound();
         }
     };
+
+    const restart = function() {
+        GameBoard.clearBoard();
+        counter = 0;
+        activePlayer = player1;
+        msg = activePlayer.name + "'s turn!";
+    }
 
     const checkHorizontal = function(board, row, letter) {
         for (let i = 0; i < 3; i++) {
@@ -139,9 +151,101 @@ const GameController = (function(playerOneName = "player1", playerTwoName = "pla
 
     return {
         playRound,
-        getActivePlayer
+        getMsg,
+        restart,
+        getActivePlayer,
+        getBoard: GameBoard.getBoard
     }
 
-})("allyssa");
+}
 
-const game = GameController;
+const ScreenController = (function(){
+    let game;
+
+    const containerDiv = document.querySelector(".container");
+    const boardDiv = document.querySelector("#board");
+    const startButton = document.getElementById("start-button");
+    const nameDialog = document.querySelector("dialog");
+    const msgDiv = document.createElement("div");
+    const okButton = document.getElementById("ok-button");
+
+    const showDialog = function() {
+        nameDialog.showModal();
+    };
+
+    const updateScreen = function() {
+
+        const board = game.getBoard();
+        const msg = game.getMsg()
+        msgDiv.textContent = msg;
+
+        // for each row
+        for (let x = 0; x < 3; x++) {
+            // for each col
+            for (let y = 0; y < 3; y++) {
+                const btn = document.querySelector(`button[data-row="${x}"][data-col="${y}"]`);
+                // console.log(`button[data-row="${x}"][data-col="${y}"]`);
+
+                if (board[x][y] !== "_") {
+                    btn.textContent = board[x][y]
+                }
+                else {
+                    btn.textContent= ""; 
+                }
+            }
+        }
+
+        if (msg != null && msg.toLowerCase().includes("winner")) {
+            // don't allow further moves
+            boardDiv.style.pointerEvents = "none";
+        }
+    }
+
+    const restartGame = function() {
+        boardDiv.style.pointerEvents = "auto";
+        game.restart();
+        updateScreen();
+    }
+
+    const clickHandlerBoard = function(event) {
+        const selectedRow = event.target.dataset.row;
+        const selectedCol = event.target.dataset.col;
+
+        if (!selectedRow || !selectedCol) return;
+
+        game.playRound([selectedCol, selectedRow]); // since board is nested array with each row as a list
+        updateScreen();
+    }
+
+   const handleSubmit = function() {
+        const form = nameDialog.querySelector("form");
+
+        const formData = new FormData(form);
+        const player1 = formData.get("player1-name");
+        const player2 = formData.get("player2-name");
+
+        game = GameController(player1, player2);
+
+        form.reset();
+    }
+
+    startButton.addEventListener("click", showDialog);
+
+    nameDialog.addEventListener("close", () => {
+        startButton.textContent = "Restart Game";
+        startButton.removeEventListener("click", showDialog);
+
+        handleSubmit();
+
+        // add msg board
+        msgDiv.className = "msg-board";
+        msgDiv.textContent = game.getMsg();
+        containerDiv.appendChild(msgDiv);
+
+        startButton.addEventListener("click", restartGame);
+    });
+
+    boardDiv.addEventListener("click", clickHandlerBoard);
+
+})();
+
